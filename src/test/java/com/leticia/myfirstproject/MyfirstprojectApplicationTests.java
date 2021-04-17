956c5032468ec315442ff01a896a6c61c884c930
leeconde <lecondesouza@gmail.com>
2021-04-17 20:14:04 -0300

    Category in memory

---
+++ src/test/java/com/leticia/myfirstproject/MyfirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leticia.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
